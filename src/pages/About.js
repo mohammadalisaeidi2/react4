c2499473d65cb64e69b88432f590874ffa0a146d
Mohammadali <mohammadalisaeidi@yahoo.com>
2022-11-20 17:25:22 +0330

    Ract router added
    E73

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+        <h2>About page</h2>
+        <p>Id cupidatat eu irure velit aliquip velit qui elit consequat.</p>
+    </div>
+  )
+}
---
+++ src/pages/Contact.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div>
+        <h2>Contact us</h2>
+        <p>Officia quis proident magna adipisicing esse do adipisicing exercitation magna culpa aliqua.</p>
+    </div>
+  )
+}
